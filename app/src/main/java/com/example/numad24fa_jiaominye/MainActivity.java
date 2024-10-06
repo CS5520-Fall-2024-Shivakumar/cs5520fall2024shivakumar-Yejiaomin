95c10908d71e7256423754f2723284ed1d037756
Amy Ye <yejiaomin@GMAIL.COM>
2024-10-06 18:16:52 -0400

    Add click listener to get the button text and display.

--- app/src/main/java/com/example/numad24fa_jiaominye/MainActivity.java
+++ app/src/main/java/com/example/numad24fa_jiaominye/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.numad24fa_jiaominye;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView display;
+
+    MaterialButton num1, num2, num3, num4, num5, num6, num7, num8, num9;
+    MaterialButton add, num0, minus, multiply, equal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        display = findViewById(R.id.display);
+        assign(num1, R.id.num1);
+        assign(num2, R.id.num2);
+        assign(num3, R.id.num2);
+        assign(num4, R.id.num4);
+        assign(num5, R.id.num5);
+        assign(num6, R.id.num6);
+        assign(num7, R.id.num7);
+        assign(num8, R.id.num8);
+        assign(num9, R.id.num9);
+        assign(num0, R.id.num0);
+        assign(add, R.id.add);
+        assign(minus, R.id.minus);
+        assign(multiply, R.id.multiply);
+        assign(equal, R.id.equal);
+    }
+    void assign (MaterialButton btn, int id) {
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        display.setText(buttonText);
     }
 }
