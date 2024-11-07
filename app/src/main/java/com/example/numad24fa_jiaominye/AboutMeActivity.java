bfe0e742b345dd93b361c3d26c7f9b60595c469d
Amy Ye <yejiaomin@GMAIL.COM>
2024-11-06 21:43:13 -0800

    Add about me layout and activity.

---
+++ app/src/main/java/com/example/numad24fa_jiaominye/AboutMeActivity.java
@@ -0,0 +1,12 @@
+package com.example.numad24fa_jiaominye;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutMeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+    }
+}
