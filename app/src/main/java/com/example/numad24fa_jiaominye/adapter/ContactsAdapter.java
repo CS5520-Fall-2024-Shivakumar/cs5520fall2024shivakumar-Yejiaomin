85d28face32f6e07e223d162c6b5723c61eb6bfb
Amy Ye <yejiaomin@GMAIL.COM>
2024-11-06 23:38:44 -0800

    Add ContactsAdapter.

---
+++ app/src/main/java/com/example/numad24fa_jiaominye/adapter/ContactsAdapter.java
@@ -0,0 +1,64 @@
+package com.example.numad24fa_jiaominye.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.numad24fa_jiaominye.ContactsActivity;
+import com.example.numad24fa_jiaominye.R;
+import com.example.numad24fa_jiaominye.model.Contact;
+
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+
+    private final List<Contact> contactList;
+    private final OnContactClickListener clickListener;
+
+
+    public ContactsAdapter(List<Contact> contactList, OnContactClickListener clickListener) {
+        this.contactList = contactList;
+        this.clickListener = clickListener;
+    }
+
+    public interface OnContactClickListener {
+        void onContactClick(Contact contact);
+    }
+
+
+    @NonNull
+    @Override
+    public ContactsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false); // 修正这里的布局文件
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsAdapter.ViewHolder holder, int position) {
+        Contact contact = contactList.get(position);
+        holder.nameTextView.setText(contact.getName());
+        holder.phoneTextView.setText(contact.getPhone());
+        holder.itemView.setOnClickListener(v -> clickListener.onContactClick(contact));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView nameTextView;
+        public TextView phoneTextView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.contact_name);
+            phoneTextView = itemView.findViewById(R.id.contact_phone);
+
+        }
+    }
+}
